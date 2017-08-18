df4e7a89ff132fd3d0bfe9ff67f5f933fe209585
heba mahmoud <heba88.mahmoud@gmail.com>
2017-08-18 19:29:46 +0200

    adding .ignore file and adding scripts correctely

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,18 +3,21 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController',LunchCheckController);
+.controller('LunchCheckController', LunchCheckController);
+
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.lunchmenu=""
-  $scope.message=""
+  $scope.message="Hello"
   $scope.checkLunchMenu = function () {
+        console.log('Check');
    //split $scope.lunchmenu if not empty
-   if   $scope.lunchmenu.length == 0 {
+   if   ($scope.lunchmenu.length == 0) {
         $scope.message="Please enter data first"
+        console.log('Empty');
    }else {
      var arrayOfStrings = $scope.lunchmenu.split(',');
-     if arrayOfStrings.length <= 3 {
+     if (arrayOfStrings.length <= 3) {
          $scope.message="Enjoy!"
      }else {
        $scope.message="Too much!"
