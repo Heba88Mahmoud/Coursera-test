7835beeb9a3286f9b82ccc542003f9979fd9c5e7
heba mahmoud <heba88.mahmoud@gmail.com>
2017-09-02 19:37:35 +0200

    assignment3 solution

---
+++ module3-solution/app.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController',NarrowItDownController)
+.service('MenuSearchService',MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController (MenuSearchService){
+ var list = this;
+ list.searchTerm = "" ;
+ //list.found = MenuSearchService.getMatchedMenuItems(list.searchTerm);
+var promise = MenuSearchService.getMatchedMenuItems();
+promise.then(function (response) {
+    list.found = response.data;
+  })
+  .catch(function (error) {
+    console.log("Something went terribly wrong.");
+  });
+
+}
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http){
+  var service = this;
+  service.getMatchedMenuItems = function(){
+    var response = $http({
+    method: "GET",
+    url: "https://davids-restaurant.herokuapp.com/menu_items.json" ,
+  });
+  return response;
+};
+}
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'menuItems.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    }
+  };
+
+  return ddo;
+}
+
+})();
