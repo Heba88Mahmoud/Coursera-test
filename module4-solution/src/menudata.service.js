719a5811947854465d5a4d4592a2cfe363a89899
heba mahmoud <heba88.mahmoud@gmail.com>
2017-09-08 18:20:54 +0200

    assignment4 solution

---
+++ module4-solution/src/menudata.service.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('data')
+.service('MenuDataService', MenuDataService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+
+
+MenuDataService.$inject = ['$http'];
+function MenuDataService($http){
+  var service = this;
+  service.getAllCategories = function(){
+    var response = $http({
+    method: "GET",
+    url: (ApiBasePath + "/categories.json")
+  });
+  return response;
+};
+
+service.getItemsForCategory = function (categoryShortName) {
+   var response = $http({
+     method: "GET",
+     url: (ApiBasePath + "/menu_items.json"),
+     params: {
+       category: categoryShortName
+     }
+   });
+
+   return response;
+ };
+
+
+}
+
+})();
---
+++ module4-solution/src/routes.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp').config(RoutesConfig);
+RoutesConfig.$inject = ['$stateProvider','$urlRouterProvider'];
+function RoutesConfig($stateProvider,$urlRouterProvider){
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+    // *** Set up UI states ***
+  $stateProvider
+    // Home page
+    .state('home',{
+             url:'/' ,
+             templateUrl: '/home.html'
+     })
+    .state('categories',{
+              url:'/categories',
+              templateUrl: '/categories.html',
+              controller: 'categoriesListController as categoriesList',
+              resolve: {
+                categories: ['MenuDataService', function (MenuDataService) {
+                return MenuDataService.getAllCategories();
+               }]
+             }
+    })
+    .state('items',{
+               url:'/items',
+               templateUrl: 'items.html'
+    });
+}
+
+})();
