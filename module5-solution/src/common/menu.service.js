bcbef85ec4a66cefd47c65c03dee9c49a0221c69
heba mahmoud <heba88.mahmoud@gmail.com>
2017-09-15 23:59:17 +0200

    module5-solution

--- module5-solution/src/common/menu.service.js
+++ module5-solution/src/common/menu.service.js
@@ -8,6 +8,7 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
+  service.user = {};
 
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
